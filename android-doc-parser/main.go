039b5ce4baa87f153e05d944b0e6f8f3a8655608
Stanislav Seletskiy <s.seletskiy@gmail.com>
2015-01-17 16:12:29 +0600

    doc parsing: ok, that was just stupid...

--- android-doc-parser/main.go
+++ android-doc-parser/main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"golang.org/x/net/html"
-)
-
-type Step string
-
-const (
-	stepStart       Step = "stepStart"
-	stepClassNav    Step = "stepClassNav"
-	stepEnd         Step = "stepEnd"
-	stepClassesList Step = "stepClassesList"
-	stepClassLink   Step = "stepClassLink"
-	stepUnknown     Step = "stepUnknown"
+	"launchpad.net/xmlpath"
 )
 
 type Class struct {
@@ -24,14 +12,8 @@ type Class struct {
 	Name string
 }
 
-type tokenizerState struct {
-	Step       Step
-	TagName    string
-	TokenType  html.TokenType
-	Attributes map[string]string
-}
-
-var classes []Class
+var xpathClassA = xmlpath.MustCompile(`//td[@class='jd-linkcol']/a`)
+var xpathClassAHref = xmlpath.MustCompile(`@href`)
 
 func main() {
 	reader, err := os.Open("package-summary.html")
@@ -39,140 +21,22 @@ func main() {
 		panic(err)
 	}
 
-	tokenizer := html.NewTokenizer(reader)
-
-	state := tokenizerState{
-		Step: stepStart,
+	root, err := xmlpath.ParseHTML(reader)
+	if err != nil {
+		panic(err)
 	}
 
-	for state.Step != "stepEnd" {
-		tokenType := tokenizer.Next()
-		tagName, hasAttr := tokenizer.TagName()
-		state.TagName = string(tagName)
-		state.TokenType = tokenType
-		state.Attributes = getAttributes(hasAttr, tokenizer)
+	classes := []Class{}
 
-		state.Step = switchState(state, tokenizer)
+	linksIterator := xpathClassA.Iter(root)
+	for linksIterator.Next() {
+		node := linksIterator.Node()
+		href, _ := xpathClassAHref.String(node)
+		classes = append(classes, Class{
+			Link: href,
+			Name: node.String(),
+		})
 	}
 
-	for _, class := range classes {
-		fmt.Printf("%60s %s\n", class.Name, class.Link)
-	}
-}
-
-func switchState(
-	state tokenizerState, tokenizer *html.Tokenizer,
-) Step {
-	//debug(state)
-
-	if state.TokenType == html.ErrorToken {
-		return "stepEnd"
-	}
-
-	switch state.Step {
-	case stepStart:
-		return parseClassNav(state, tokenizer)
-	case stepClassNav:
-		return parseClassesBegin(state, tokenizer)
-	case stepClassesList:
-		return parseClassesList(state, tokenizer)
-	case stepClassLink:
-		return parseClassLink(state, tokenizer)
-	}
-
-	return stepUnknown
-}
-
-func parseClassNav(
-	state tokenizerState, tokenizer *html.Tokenizer,
-) Step {
-	nextStep := state.Step
-
-	switch state.TokenType {
-	case html.StartTagToken:
-		if state.Attributes["id"] == "classes-nav" {
-			nextStep = stepClassLink
-		}
-	}
-
-	return nextStep
-}
-
-func parseClassesBegin(
-	state tokenizerState, tokenizer *html.Tokenizer,
-) Step {
-	nextStep := state.Step
-
-	switch state.TokenType {
-	case html.TextToken:
-		text := string(tokenizer.Text())
-		if text == "Classes" {
-			nextStep = stepClassesList
-		}
-	}
-
-	return nextStep
-}
-
-func parseClassesList(
-	state tokenizerState, tokenizer *html.Tokenizer,
-) Step {
-	nextStep := state.Step
-
-	switch state.TokenType {
-	case html.StartTagToken:
-		if state.Attributes["class"] == "jd-linkcol" {
-			nextStep = stepClassLink
-		}
-	}
-
-	return nextStep
-}
-
-func parseClassLink(
-	state tokenizerState, tokenizer *html.Tokenizer,
-) Step {
-	nextStep := state.Step
-
-	switch state.TokenType {
-	case html.StartTagToken:
-		if state.TagName == "a" {
-			href := state.Attributes["href"]
-			tokenizer.Next()
-			name := string(tokenizer.Text())
-			classes = append(classes, Class{
-				Link: href,
-				Name: name,
-			})
-		}
-	case html.EndTagToken:
-		nextStep = stepClassesList
-	}
-
-	return nextStep
-}
-
-func getAttributes(hasAttr bool, tokenizer *html.Tokenizer) map[string]string {
-	attributes := make(map[string]string)
-
-	for hasAttr {
-		key, value, hasMoreAttr := tokenizer.TagAttr()
-
-		attributes[string(key)] = string(value)
-
-		if !hasMoreAttr {
-			break
-		}
-	}
-
-	return attributes
-}
-
-func debug(state tokenizerState) {
-	switch state.TokenType {
-	case html.StartTagToken:
-		log.Printf("%s <%s>", state.Step, state.TagName)
-	case html.EndTagToken:
-		log.Printf("%s </%s>", state.Step, state.TagName)
-	}
+	fmt.Printf("%+v\n", classes)
 }
